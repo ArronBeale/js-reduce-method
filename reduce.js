f846677a129adc85d19c06110b32206e570ed60d
Arron <beale86@msn.com>
2022-06-25 17:48:05 +0000

    Add usage example of the reduce method on an array of objects

--- reduce.js
+++ reduce.js
@@ -54,3 +54,28 @@ const teamMembers = [
 let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0); 
 console.log(totalExperience);
 // Grouping by a property, and totaling it too
+// {Developer: 12, Designer: 4} <----- this is what we want
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(experienceByProfession);
+
+// Usage example on an array or objects to get the highest score in english
+// result we want {name: 'Adam', max: 88}
+const biggest = students.reduce((acc, curr) => {
+  if (curr.results.english > acc.max) {
+      acc.max= curr.results.english;
+      acc.name= curr.name
+  } 
+  return acc;
+},{name: '', max:0},);
+
+
+console.log(biggest);
